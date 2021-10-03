843af72dcafb5317ddd400fa428db3a0a0ffe3e5
albechen <59457095+albechen@users.noreply.github.com>
2021-10-03 01:55:21 -0400

    feat: 05 smallest number

--- 05-Smallest_multiple.py
+++ 05-Smallest_multiple.py
@@ -9,14 +9,48 @@ all of the numbers from 1 to 20?
 """
 
 #%%
-topNumber = 20
-currentNumber = topNumber
-for x in range(1, topNumber+1)
-    if currentNumber % 2 
-if currentNumber % 2:
-    n += 2
-else:
-    n += 1
-#%%
-list(range(1,21))
+def return_prime_factors(number):
+    currentNumber = number
+    fullPrimeFactors = False
+    primeFactorList = []
+    n = 2
+
+    while fullPrimeFactors == False:
+        if currentNumber % n == 0:
+            currentNumber = currentNumber / n
+            primeFactorList.append(n)
+            if int(multiply_list(primeFactorList)) == number:
+                fullPrimeFactors = True
+        if n == 2:
+            n = n + 1
+        else:
+            n = n + 2
+    return primeFactorList
+
+
+def multiply_list(list):
+    product = 1
+    for x in list:
+        product = product * x
+    return product
+
+
+def smallest_divisable_num(highestNumber):
+    primeFactorList = []
+    for n in range(2, highestNumber + 1):
+        print(n)
+        nPrimeFactors = return_prime_factors(n)
+        print(nPrimeFactors)
+        primeFactorList = primeFactorList + list(
+            set(nPrimeFactors) - set(primeFactorList)
+        )
+    smallestDivisable = multiply_list(primeFactorList)
+    return smallestDivisable
+
+
+smallest_divisable_num(20)
+
+# %%
+return_prime_factors(50)
+
 # %%
