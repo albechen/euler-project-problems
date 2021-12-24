e82c9737a9393f0a1118b976a4bf12cf828c7e53
albechen <59457095+albechen@users.noreply.github.com>
2021-12-24 03:41:49 -0500

    doc: reorginize files

--- problems/01-09/06-sum_square_difference.py
+++ problems/01-09/06-sum_square_difference.py
@@ -9,8 +9,19 @@ The square of the sum of the first ten natural numbers is,
 
 Hence the difference between the sum of the squares of the first
 ten natural numbers and the square of the sum is .
-3025 - 385
+3025 - 385 = 2640
 
 Find the difference between the sum of the squares of the first
 one hundred natural numbers and the square of the sum.
 """
+
+#%%
+def diff_sqrSum_sumSqr(n):
+    sumSqr = sum([x ** 2 for x in range(1, n + 1)])
+    sqrSum = sum(range(1, n + 1)) ** 2
+    diff = sqrSum - sumSqr
+    return diff
+
+
+n = 100
+diff_sqrSum_sumSqr(n)
